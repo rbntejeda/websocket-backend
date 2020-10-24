e6653543580ca1b11e19176fe69b3882b2af3d23
Ruben Eduardo Tejeda Roa <rubentejedaroa@gmail.com>
2020-10-24 18:23:24 -0300

    update message

--- src/index.js
+++ src/index.js
@@ -7,13 +7,18 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+  socket.broadcast.emit('hi');
+
   console.log('a user connected');
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
+
   socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
+
 });
 
 http.listen(3000, () => {
