03a523315346ceea9b2c62b846085056973c1b8b
Ruben Eduardo Tejeda Roa <rubentejedaroa@gmail.com>
2020-10-24 18:06:04 -0300

    add template and nodemon dev depency

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ var app = require('express')();
 var http = require('http').createServer(app);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+    res.sendFile(__dirname + '/template/index.html');
+  });
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
