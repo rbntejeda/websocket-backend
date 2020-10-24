5fc97f02df8dfa554231b8a04b4a4b37e9434ddd
Ruben Eduardo Tejeda Roa <rubentejedaroa@gmail.com>
2020-10-24 17:43:47 -0300

    add express example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-var io = require('socket.io')(80);
-var cfg = require('./config.json');
-var tw = require('node-tweet-stream')(cfg);
-tw.track('socket.io');
-tw.track('javascript');
-tw.on('tweet', function(tweet){
-  io.emit('tweet', tweet);
+var app = require('express')();
+var http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
 });
